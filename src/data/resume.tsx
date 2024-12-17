1c80cf2ec6fde57991b46c752107f3e4c0540710
sandeepyadav-24 <103882286+sandeepyadav-24@users.noreply.github.com>
2024-12-17 21:49:53 +0530

    First Commit

---
+++ src/data/resume.tsx
@@ -0,0 +1,415 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Sandeep Yadav",
+  initials: "SY",
+  url: "https://sandeepyadav.io",
+  location: "Noida, India",
+  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  description:
+    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+  summary:
+    "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
+  avatarUrl: "https://avatars.githubusercontent.com/u/103882286?v=4",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+
+    "Go",
+    "Postgres",
+    "Docker",
+    "Kubernetes",
+
+    "C++",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+  ],
+  contact: {
+    email: "sandeepyadav004343@gmail.com",
+    tel: "+8076846925",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/sandeepyadav-24",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/sandeep-yadav-027500219/",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://x.com/sandeepyadav_24",
+        icon: Icons.x,
+
+        navbar: true,
+      },
+      Youtube: {
+        name: "Youtube",
+        url: "https://www.youtube.com/@sandeepyadav.24",
+        icon: Icons.youtube,
+        navbar: true,
+      },
+      email: {
+        name: "sandeep yadav",
+        url: "#sandeepyadav004343@gmail.com",
+        icon: Icons.email,
+
+        navbar: false,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Atomic Finance",
+      href: "https://atomic.finance",
+      badges: [],
+      location: "Remote",
+      title: "Bitcoin Protocol Engineer",
+      logoUrl: "/atomic.png",
+      start: "May 2021",
+      end: "Oct 2022",
+      description:
+        "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
+    },
+
+    {
+      company: "Mitre Media",
+      href: "https://mitremedia.com/",
+      badges: [],
+      location: "Toronto, ON",
+      title: "Software Engineer",
+      logoUrl: "/mitremedia.png",
+      start: "May 2017",
+      end: "August 2017",
+      description:
+        "Designed and implemented a robust password encryption and browser cookie storage system in Ruby on Rails. Leveraged the Yahoo finance API to develop the dividend.com equity screener",
+    },
+  ],
+  education: [
+    {
+      school: "ITS Engineering College, Greater Noida",
+      href: "https://uwaterloo.ca",
+      degree: "Bachelor's Degree of Computer Science (BCS)",
+      logoUrl: "/waterloo.png",
+      start: "2016",
+      end: "2021",
+    },
+    {
+      school: "Lingaya's Public School, Faridabad",
+      href: "https://wlu.ca",
+      degree: "Higher Secondary School",
+      logoUrl: "/laurier.png",
+      start: "2016",
+      end: "2021",
+    },
+    {
+      school: "The Cambridge International School, South delhi",
+      href: "https://ibo.org",
+      degree: "Secondary School",
+      logoUrl: "/ib.png",
+      start: "2012",
+      end: "2016",
+    },
+  ],
+  projects: [
+    {
+      title: "Chat Collect",
+      href: "https://chatcollect.com",
+      dates: "Jan 2024 - Feb 2024",
+      active: true,
+      description:
+        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://chatcollect.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+    },
+    {
+      title: "Magic UI",
+      href: "https://magicui.design",
+      dates: "June 2023 - Present",
+      active: true,
+      description:
+        "Designed, developed and sold animated UI components for developers.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://magicui.design",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/magicuidesign/magicui",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://cdn.magicui.design/bento-grid.mp4",
+    },
+    {
+      title: "llm.report",
+      href: "https://llm.report",
+      dates: "April 2023 - September 2023",
+      active: true,
+      description:
+        "Developed an open-source logging and analytics platform for OpenAI: Log your ChatGPT API requests, analyze costs, and improve your prompts.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Magic UI",
+        "Stripe",
+        "Cloudflare Workers",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://llm.report",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/dillionverma/llm.report",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://cdn.llm.report/openai-demo.mp4",
+    },
+    {
+      title: "Automatic Chat",
+      href: "https://automatic.chat",
+      dates: "April 2023 - March 2024",
+      active: true,
+      description:
+        "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Magic UI",
+        "Stripe",
+        "Cloudflare Workers",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://automatic.chat",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/automatic-chat.mp4",
+    },
+  ],
+  hackathons: [
+    {
+      title: "Hack With Delhi",
+      dates: "May 20th, 2024",
+      location: "GL Bajaj INstitute of technology, Greater Noida",
+      description: "Developed neural network to optimize a mining process",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-mining.png",
+      links: [],
+    },
+    {
+      title: "Code Cubicle",
+      dates: "May 15th, 2024",
+      location: "Eccosphere, Noida",
+      description: "Developed neural network to optimize a mining process",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-mining.png",
+      links: [],
+    },
+    {
+      title: "Hack With India",
+      dates: "April 6th, 2024",
+      location: "Microsoft, Gurugram",
+      description: "Developed neural network to optimize a mining process",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-mining.png",
+      links: [],
+    },
+    {
+      title: "Hack Hound 2.0",
+      dates: "April 5th, 2024",
+      location: "SRM, Ghaziabad",
+      description:
+        "Developed Pocketdoc, an app in which you take a picture of a physical wound, and the app returns common solutions or cures to the injuries or diseases.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/waterloo-equithon.png",
+      links: [
+        {
+          title: "Devpost",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://devpost.com/software/pocketdoc-react-native",
+        },
+        {
+          title: "YouTube",
+          icon: <Icons.youtube className="h-4 w-4" />,
+          href: "https://www.youtube.com/watch?v=XwFdn5Rmx68",
+        },
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/pocketdoc-react-native",
+        },
+      ],
+    },
+    {
+      title: "Electrothon 6.0",
+      dates: "March 8th, 2024",
+      location: "NIT Hamirpur, Himachal Pradesh",
+      description:
+        "Developed Earthwatch, a web application which allows users in a plane to virtually see important points of interest about the world below them. They can even choose to fly away from their route and then fly back if they choose. Special thanks to CesiumJS for providing open source world and plane models.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/space-apps.png",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/earthwatch",
+        },
+      ],
+    },
+    {
+      title: "Hacked 2.0",
+      dates: "March 1th, 2024",
+      location: "BML Munjal University, Haryana",
+      description:
+        "Developed Super Graphic Air Traffic, a VR website made to introduce people to the world of air traffic controlling. This project was built completely using THREE.js as well as a node backend server.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/mhacks-9.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2017/white.svg",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/threejs-planes",
+        },
+      ],
+    },
+    {
+      title: "Open Hack",
+      dates: "February 24th, 2024",
+      location: "IISC, Bangalore",
+      description:
+        "Developed at StartHacks 2017, Recipic is a mobile app which allows you to take pictures of ingredients around your house, and it will recognize those ingredients using ClarifAI image recognition API and return possible recipes to make. Recipic recieved 1st place at the hackathon for best pitch and hack.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/starthacks.png",
+      win: "1st Place Winner",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2017/white.svg",
+      links: [
+        {
+          title: "Source (Mobile)",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/mattBlackDesign/recipic-ionic",
+        },
+        {
+          title: "Source (Server)",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/mattBlackDesign/recipic-rails",
+        },
+      ],
+    },
+    {
+      title: "Hack Street",
+      dates: "February 7th, 2024",
+      location: "Jaypee Institute Of Information Technology, Noida",
+      description:
+        "Developed a mobile game which enables city-wide manhunt with random lobbies",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/qhacks.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2017/white.svg",
+      links: [
+        {
+          title: "Source (Mobile)",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/human-huntr-react-native",
+        },
+        {
+          title: "Source (API)",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/mattBlackDesign/human-huntr-rails",
+        },
+      ],
+    },
+    {
+      title: "Smart India hackathon",
+      dates: "November 7th, 2023",
+      location: "ITS Engineering College, Greater Noida",
+      description:
+        "Developed a mock of Windows 11 with interesting notifications and functionality",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/terrible-hacks-v.png",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/justinmichaud/TerribleHacks2016-Windows11",
+        },
+      ],
+    },
+    {
+      title: "Hack Xtreme",
+      dates: "October 5, 2023",
+      location: "IIT Delhi",
+      description:
+        "Developed an internal widget for uploading assignments using Waterloo's portal app",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/portal-hackathon.png",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/UWPortalSDK/crowmark",
+        },
+      ],
+    },
+  ],
+} as const;
